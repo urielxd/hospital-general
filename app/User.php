be0ed88d92e687db6d0f2829f3ef62346ded9dae
carlos-stgo <carlos.u.santiago@icloud.com>
2018-12-09 21:55:30 -0600

    Curp

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar', 'role'
+        'name', 'email', 'password', 'avatar', 'role', 'curp'
     ];
 
     /**
